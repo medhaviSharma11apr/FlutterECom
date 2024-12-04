9c68142691ac6d7244d58c0498bcdc9727928a9a
medhaviSharma11apr <medhavisharma11apr@gmail.com>
2024-12-04 17:23:40 +0530

    first commit

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: 
+  );
+  static ThemeData darkTheme = ThemeData();
+}
